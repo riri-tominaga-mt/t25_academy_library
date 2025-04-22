6cc5bad5a46094446147ee5975d543be8ea1b69b
riri-tominaga-mt <riri.tominaga@metateam.co.jp>
2025-04-22 20:10:23 +0900

    BookMst //変更

--- src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
+++ src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
@@ -3,6 +3,7 @@ package jp.co.metateam.library.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import jp.co.metateam.library.model.Account;
 import jp.co.metateam.library.model.BookMst;
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +15,9 @@ public interface BookMstRepository extends JpaRepository<BookMst, Long> {
 
 	@Query(value = "SELECT * FROM book_mst WHERE id = ?1", nativeQuery = true)
 	Optional<BookMst> selectById(Long id);
+
+	@Query(value = "SELECT* FROM book_mst WHERE ISBN LIKE %:ISBN%",nativeQuery = true)
+	List<BookMst>selectByIsbn(String ISBN);
+
 }
+
